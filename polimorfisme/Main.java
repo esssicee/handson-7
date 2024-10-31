69988c511d33aff0504dfb4c9ab15bfe4ae172bf
gospel <2381043@unai.edu>
2024-10-31 21:08:33 +0700

    Handson 7

--- polimorfisme/Main.java
+++ polimorfisme/Main.java
@@ -1,6 +1,9 @@
 package polimorfisme;
 
 public class Main {
+    private static double v;
+    private static String persegi;
+
     public static void main(String[] args) {
         BangunDatar bangunDatar;
 
@@ -13,5 +16,10 @@ public class Main {
         bangunDatar = new Lingkaran(4);
         tampilkanPerhitungan(bangunDatar.hitungLuas(), bangunDatar.hitungKeliling(), "Lingkaran");
 
+    }
+
+    private static void tampilkanPerhitungan(double v, double v1, String persegi) {
+        Main.v = v;
+        Main.persegi = persegi;
     }
 }
